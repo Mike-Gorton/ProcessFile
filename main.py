83c5f3604285705b4c3c0c7fb7a342fe37a1e299
Mike <mike@gorton.za.net>
2023-01-15 12:07:42 +0200

    MCG: Update and corrections

--- main.py
+++ main.py
@@ -10,19 +10,20 @@ import psycopg2
 from psycopg2.extras import Json
 
 BASE_DIR = pathlib.Path(__file__).parent.resolve()
-datadir = f"{BASE_DIR}/files"
+data_dir = f"{BASE_DIR}/files"
 
-def openConn():
+
+def open_connection():
     conn = None
     try:
-        # connect to the PostgreSQL server
-        print('Connecting to the PostgreSQL database...')
+        # connect to the PostgresSQL server
+        print('Connecting to the PostgresSQL database...')
         # connection establishment
-        conn = psycopg2.connect( database="pi", user='pi', password='pi', host='192.168.1.44', port='5432')
+        conn = psycopg2.connect(database="pi", user='pi', password='pi', host='192.168.1.44', port='5432')
         cur = conn.cursor()
         cur.execute('SELECT version()')
-        print('CONNECTION OPEN:PostgreSQL database version:')
-        # display the PostgreSQL database server version
+        print('CONNECTION OPEN:PostgresSQL database version:')
+        # display the PostgresSQL database server version
         db_version = cur.fetchone()
         print(db_version)
 
@@ -31,25 +32,26 @@ def openConn():
 
     return conn
 
-def closeConn(conn):
+
+def close_connection(conn):
     try:
         conn.close()
-        print('Closed connection to the PostgreSQL database')
+        print('Closed connection to the PostgresSQL database')
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
     finally:
         if conn is not None:
             conn.close()
-print('Database connection closed.')
+            print('Database connection closed.')
 
 
 def process_file():
 
-    conn = openConn()
-    for x in os.listdir(datadir):
+    conn = open_connection()
+    for x in os.listdir(data_dir):
         print(x)
         if os.path.splitext(x)[1] == ".txt" and x.startswith("GHQ"):
-            fullname = os.path.join(datadir, x)
+            fullname = os.path.join(data_dir, x)
             with open(f"{fullname}", "r") as file:
                 content = file.readlines()
                 for line in content:
@@ -62,29 +64,32 @@ def process_file():
                     conn.commit()
             file.close()
             os.remove(fullname)
-    closeConn(conn)
+    close_connection(conn)
 
 
 def sql_insert(cursor, dictionary):
 
-    job=dictionary['job']
-    host=dictionary['host']
-    timestamp=dictionary['timestamp']
-    status=dictionary['status']
-    sub_job=dictionary['sub_job']
-    temprature=to_float((dictionary['temperature'][:4]))
+    job = dictionary['job']
+    host = dictionary['host']
+    timestamp = dictionary['timestamp']
+    status = dictionary['status']
+    sub_job = dictionary['sub_job']
+    temperature = to_float((dictionary['temperature'][:4]))
 
     sql = '''
     INSERT INTO device_log (
     devlog_ip, devlog_job, devlog_timestamp, devlog_status, devlog_subjob, devlog_temprature)
     VALUES( %s,%s,%s,%s,%s,%s) '''
 
-    print(dictionary["job"],dictionary["host"],dictionary["timestamp"],dictionary["status"],dictionary["sub_job"],dictionary["temperature"])
-    cursor.execute(sql, (str(host) ,str(job), str(timestamp), str(status), str(sub_job), str(temprature)))
+    print(dictionary["job"], dictionary["host"], dictionary["timestamp"], dictionary["status"], dictionary["sub_job"],
+          dictionary["temperature"])
+    cursor.execute(sql, (str(host), str(job), str(timestamp), str(status), str(sub_job), str(temperature)))
+
 
 def to_float(s):
     n = float(s)
-    return  n
+    return n
+
 
 if __name__ == '__main__':
     process_file()
