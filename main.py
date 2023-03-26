4e850fb1d3c7cd416c11a5d9426a1eadc5aa02c8
Mike-Gorton <mike@gorton.za.net>
2023-03-26 13:52:56 +0200

    Changes to determination of file location,deployment and add logging

--- main.py
+++ main.py
@@ -8,34 +8,33 @@ import json
 import pathlib
 import psycopg2
 import configparser
+from IoT_Logger import api_logger
 
-from psycopg2.extras import Json
+def get_configuration():
+    global host
+    config = configparser.ConfigParser()
+    config.read('IoT.ini')
+    host = config['configuration']['filelocation']
 
-config = configparser.ConfigParser()
-config.read('IoT.ini')
-
-host = config['configuration']['filelocation']
-
-BASE_DIR = pathlib.Path(__file__).parent.resolve()
-data_dir = f"{BASE_DIR}/files"
-
+    BASE_DIR = pathlib.Path(__file__).parent.resolve()
+    data_dir = f"{BASE_DIR}/files"
 
 def open_connection():
     conn = None
     try:
         # connect to the PostgresSQL server
-        print('Connecting to the PostgresSQL database...')
+        api_logger.info('Connecting to the PostgresSQL database...')
         # connection establishment
         conn = psycopg2.connect(database="pi", user='pi', password='pi', host='192.168.1.44', port='5432')
         cur = conn.cursor()
         cur.execute('SELECT version()')
-        print('CONNECTION OPEN:PostgresSQL database version:')
+        api_logger.info('CONNECTION OPEN:PostgresSQL database version:')
         # display the PostgresSQL database server version
         db_version = cur.fetchone()
-        print(db_version)
+        api_logger.info(db_version)
 
     except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
+        api_logger.error(error)
 
     return conn
 
@@ -43,19 +42,18 @@ def open_connection():
 def close_connection(conn):
     try:
         conn.close()
-        print('Closed connection to the PostgresSQL database')
+        api_logger.info('Closed connection to the PostgresSQL database')
     except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
+        api_logger.error(error)
     finally:
         if conn is not None:
             conn.close()
-            print('Database connection closed.')
-
+            api_logger.info('Database connection closed.')
 
 def process_file():
 
     conn = open_connection()
-    print('Directory to read ', host)
+    api_logger.info('Directory to read ' + host)
     for x in os.listdir(host):
         print(x)
         if os.path.splitext(x)[1] == ".txt" and x.startswith("GHQ"):
@@ -64,11 +62,11 @@ def process_file():
                 content = file.readlines()
                 for line in content:
                     line = line.strip('\n')
-                    print(line)
+                    api_logger.info(line)
                     try:
                         json_data = json.loads(line)
                     except json.decoder.JSONDecodeError:
-                        print('Decode Error on file: ', x)
+                        api_logger.info('Decode Error on file: ', x)
                         break
                     cursor = conn.cursor()
                     sql_insert(cursor, json_data)
@@ -76,8 +74,6 @@ def process_file():
             file.close()
             os.remove(fullname)
     close_connection(conn)
-
-
 def sql_insert(cursor, dictionary):
 
     job = dictionary['job']
@@ -85,22 +81,22 @@ def sql_insert(cursor, dictionary):
     timestamp = dictionary['timestamp']
     status = dictionary['status']
     sub_job = dictionary['sub_job']
-    temperature = to_float((dictionary['temperature'][:4]))
+
+    if float(dictionary['temperature']):
+        temperature = float((dictionary['temperature']))
+    else:
+        temperature = float('0.00')
 
     sql = '''
     INSERT INTO device_log (
     devlog_ip, devlog_job, devlog_timestamp, devlog_status, devlog_subjob, devlog_temprature)
     VALUES( %s,%s,%s,%s,%s,%s) '''
 
-    print('Data Dump', dictionary["job"], dictionary["host"], dictionary["timestamp"], dictionary["status"], dictionary["sub_job"],
+    api_logger.info('Data Dump' + dictionary["job"] + dictionary["host"] + dictionary["timestamp"] + dictionary["status"] + dictionary["sub_job"] +
           dictionary["temperature"])
     cursor.execute(sql, (str(host), str(job), str(timestamp), str(status), str(sub_job), str(temperature)))
 
-
-def to_float(s):
-    n = float(s)
-    return n
-
-
 if __name__ == '__main__':
+    global host
+    get_configuration()
     process_file()
